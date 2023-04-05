90a1738639932e8e62cfcfc179be1c8cdcaa2eff
utkarsh singh <utkarshsinghrajput16@gmail.com>
2023-04-05 05:52:56 +0530

    Commit

--- app/src/main/java/com/orion/newsapp/Presentation/viewmodel/NewsViewmodel.kt
+++ app/src/main/java/com/orion/newsapp/Presentation/viewmodel/NewsViewmodel.kt
@@ -8,28 +8,36 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.launchIn
 
 
 @HiltViewModel
 class NewsViewmodel@Inject constructor(private val getNewsArticleUseCase: GetNewsArticleUseCase):
     ViewModel() {
-    val articles = mutableListOf(HomeStateHolder())
-
+    val articles = mutableStateOf(HomeStateHolder())
+    init {
+        getNewsArticles()
+    }
 
     fun getNewsArticles(){
         getNewsArticleUseCase().onEach {
 
             when(it){
                 is Resource.Loading->{
-
+                    articles.value = HomeStateHolder(isLoading = true)
                 }
                 is Resource.Success->{
-
+                    articles.value = HomeStateHolder(data = it.data)
                 }
-                is Resource.Error->{}
+                is Resource.Error->{
+                    articles.value = HomeStateHolder(error = it.message.toString())
+                }
             }
 
-        }
+        }.launchIn(viewModelScope)
     }
 }
